80f74d7913c8d2bb6390ed099e60405d2eb78b5a
Shalva Jashiashvili <sh.jashiashvili@gmail.com>
2017-04-07 17:39:00 +0400

    finished

--- bookmarks_example/account/views.py
+++ bookmarks_example/account/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 from django.views.decorators.http import require_POST
 
 from common.decorators import ajax_required
+from actions.utils import create_action
+from actions.models import Action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -16,7 +18,14 @@ from .models import Profile, Contact
 
 @login_required
 def dashboard(request):
-    return render(request,'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids).select_related('user', 'user__profile').prefetch_related('target')
+
+    actions = actions[:10]
+    return render(request,'account/dashboard.html', {'section': 'dashboard', 'actions': actions })
 
 
 def register(request):
@@ -26,6 +35,7 @@ def register(request):
         new_user.set_password(user_form.cleaned_data['password'])
         new_user.save()
         profile = Profile.objects.create(user=new_user)
+        create_action(new_user, 'has created an account')
         return render(request, 'account/register_done.html', {'new_user': new_user})
     return render(request, 'account/register.html', {'user_form': user_form})
 
@@ -70,8 +80,10 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,user_to=user).delete()
+                create_action(request.user, 'unfollowed', user)
             return JsonResponse({'status':'ok'})
         except User.DoesNotExist:
             return JsonResponse({'status':'ko'})
--- bookmarks_example/images/views.py
+++ bookmarks_example/images/views.py
@@ -8,6 +8,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
 from common.decorators import ajax_required
+from actions.utils import create_action
 
 
 @login_required
@@ -18,6 +19,7 @@ def image_create(request):
         new_item = form.save(commit=False)
         new_item.user = request.user
         new_item.save()
+        create_action(request.user, 'bookmarked image', new_item)
         messages.success(request, 'Image added successfully')
         return redirect(new_item.get_absolute_url())
     return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
@@ -39,8 +41,10 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
+                create_action(request.user, 'unlikes', image)
             return JsonResponse({'status':'ok'})
         except:
             pass
